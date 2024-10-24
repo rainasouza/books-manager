8b61bb44659e4aafcbdd9cc2430dbff98db62d66
Raína Souza <raiina.py@gmail.com>
2024-10-24 17:57:28 -0300

    fix css books

--- src/Books/Books.jsx
+++ src/Books/Books.jsx
@@ -1,13 +1,19 @@
 import React, { useState } from 'react'; 
 import axios from 'axios'; 
-import { Button, Card, Row, Col, Form, Alert, Spinner } from 'react-bootstrap'; 
+import { Button, Card, Row, Col, Form, Alert, Spinner, Modal } from 'react-bootstrap'; 
 import './Books.css';
 
 const Books = ({ addBook }) => { 
   const [query, setQuery] = useState(''); 
   const [books, setBooks] = useState([]); 
   const [error, setError] = useState(''); 
-  const [loading, setLoading] = useState(false); 
+  const [loading, setLoading] = useState(false);
+  
+  //modal, react bootstrap
+  const [selectedBook, setSelectedBook] = useState(null)
+  const handleClose = () => setSelectedBook(null);
+  const handleShow = (book) => setSelectedBook(book);
+
 
   const handleSearch = async (e) => { 
     e.preventDefault(); 
@@ -39,51 +45,78 @@ const Books = ({ addBook }) => {
   };
 
   return ( 
-    <div className="mini-biblioteca"> 
-      <div className="search-card"> 
-        <Form className="d-flex mb-2" onSubmit={handleSearch}> 
-          <Form.Control 
-            type="text" 
-            placeholder="Procure por autor ou nome do livro" 
-            value={query} 
-            onChange={(e) => setQuery(e.target.value)} 
-            className="search-input" 
-          /> 
-          <Button variant="primary" type="submit" className="button-search"> 
-            Buscar 
-          </Button> 
-        </Form> 
-      </div> 
+    <div className="mini-biblioteca">
+      <div className="search-card">
+        <Form className="d-flex mb-2" onSubmit={handleSearch}>
+          <Form.Control
+            type="text"
+            placeholder="Procure por autor ou nome do livro"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            className="search-input"
+          />
+          <Button variant="primary" type="submit" className="button-search">
+            Buscar
+          </Button>
+        </Form>
+      </div>
 
-      {loading && <Spinner animation="border" className="spinner-border" />} 
+      {loading && <Spinner animation="border" className="spinner-border" />}
 
-      {error && ( 
-        <Alert variant="danger" className="mt-3"> 
-          {error} 
-        </Alert> 
-      )} 
+      {error && (
+        <Alert variant="danger" className="mt-3">
+          {error}
+        </Alert>
+      )}
 
-      <Row className="mt-5" xs={2} sm={2} md={3} lg={4}> 
-        {books.map((book) => ( 
-          <Col key={book.id} className="mb-4"> 
-            <Card className="book-card"> 
-              <Card.Img 
-                variant="top" 
-                src={book.volumeInfo.imageLinks?.thumbnail || 'https://via.placeholder.com/128x193'} 
-                alt={book.volumeInfo.title} 
-              /> 
-              <Card.Body> 
-                <Card.Title className="card-title">{book.volumeInfo.title}</Card.Title> 
-                <Card.Text className="card-text">{book.volumeInfo.authors?.join(', ') || 'Autor desconhecido'}</Card.Text> 
-                <Button variant="primary" onClick={() => addBook(book)}> 
-                  Adicionar à Biblioteca 
-                </Button> 
-              </Card.Body> 
-            </Card> 
-          </Col> 
-        ))} 
-      </Row> 
-    </div> 
+      {/* Corpo de cada livro */}
+      <Row className="mt-5" xs={2} sm={2} md={4} lg={5}>
+        {books.map((book) => (
+          <Col key={book.id} className="mb-4">
+            <Card className="book-card">
+              <Card.Img
+                variant="top"
+                src={book.volumeInfo.imageLinks?.thumbnail || 'https://via.placeholder.com/128x193'}
+                alt={book.volumeInfo.title}
+              />
+              <Card.Body>
+                <Card.Title className="card-title">{book.volumeInfo.title}</Card.Title>
+                <Card.Text className="card-text">
+                  {book.volumeInfo.authors?.join(', ') || 'Autor desconhecido'}
+                </Card.Text>
+
+                {/* Botão para abrir o modal */}
+                <Button className="primary-button" onClick={() => handleShow(book)}>
+                  Informações
+                </Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+
+      {/* Modal para o livro selecionado */}
+      {selectedBook && (
+        <Modal show={true} onHide={handleClose} centered>
+          <Modal.Header closeButton className="modal-header">
+            <Modal.Title>{selectedBook.volumeInfo.title}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body className="modal-body">
+            <h6>{selectedBook.volumeInfo.publisher}, {selectedBook.volumeInfo.publishedDate}</h6>
+            <p>{selectedBook.volumeInfo.description || 'Descrição não disponível'}</p>
+            <h6>{selectedBook.volumeInfo.pageCount} páginas</h6>
+          </Modal.Body>
+          <Modal.Footer className="modal-footer">
+            <Button className="secondary-button" onClick={handleClose}>
+              Fechar
+            </Button>
+            <Button className="primary-button" onClick={() => addBook(selectedBook)}>
+              Adicionar à Biblioteca
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      )}
+    </div>
   ); 
 };
 
--- src/Library/Library.jsx
+++ src/Library/Library.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
-import { Button, Card, Row, Col, Form } from 'react-bootstrap';
+import { Button, Card, Row, Col, Modal} from 'react-bootstrap';
 
 const Library = ({library}) => {
+  const [selectedBook, setSelectedBook] = useState(null)
+  const handleClose = () => setSelectedBook(null);
+  const handleShow = (book) => setSelectedBook(book);
 
 
   return (
@@ -10,24 +13,53 @@ const Library = ({library}) => {
       {library.length === 0 ? (
         <p>Nenhum livro adicionado ainda.</p>
       ) : (
-        <Row>
-          {library.map((book) => (
-            <Col md={4} key={book.id} className="mb-4">
-              <Card className="library-book-card">
-                <Card.Img
-                  variant="top"
-                  src={book.volumeInfo.imageLinks?.thumbnail || 'https://via.placeholder.com/128x193'}
-                  alt={book.volumeInfo.title}
-                />
-                <Card.Body>
-                  <Card.Title>{book.volumeInfo.title}</Card.Title>
-                  <Card.Text>{book.volumeInfo.authors?.join(', ') || 'Autor desconhecido'}</Card.Text>
-                </Card.Body>
-              </Card>
-            </Col>
-            
-          ))}
-        </Row>
+    
+  <div>
+      <Row className="mt-5" xs={2} sm={2} md={4} lg={5}>
+        {library.map((book) => (
+          <Col key={book.id} className="mb-4">
+            <Card className="book-card">
+              <Card.Img
+                variant="top"
+                src={book.volumeInfo.imageLinks?.thumbnail || 'https://via.placeholder.com/128x193'}
+                alt={book.volumeInfo.title}
+              />
+              <Card.Body>
+                <Card.Title className="card-title">{book.volumeInfo.title}</Card.Title>
+                <Card.Text className="card-text">
+                  {book.volumeInfo.authors?.join(', ') || 'Autor desconhecido'}
+                </Card.Text>
+
+                {/* Botão para abrir o modal */}
+                <Button className="primary-button" onClick={() => handleShow(book)}>
+                  Informações
+                </Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+
+      
+      {selectedBook && (
+        <Modal show={true} onHide={handleClose} centered>
+          <Modal.Header closeButton className="modal-header">
+            <Modal.Title>{selectedBook.volumeInfo.title}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body className="modal-body">
+            <h6>{selectedBook.volumeInfo.publisher}, {selectedBook.volumeInfo.publishedDate}</h6>
+            <p>{selectedBook.volumeInfo.description || 'Descrição não disponível'}</p>
+            <h6>{selectedBook.volumeInfo.pageCount} páginas</h6>
+          </Modal.Body>
+          <Modal.Footer className="modal-footer">
+            <Button className="secondary-button" onClick={handleClose}>
+              Fechar
+            </Button>
+
+          </Modal.Footer>
+        </Modal>
+      )}
+      </div>
       )}
     </div>
   )
